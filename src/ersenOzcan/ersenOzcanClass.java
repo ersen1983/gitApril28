069991038374eebefb8a276cc0fd56c61bf17bc3
ersen ozcan <ersen2008@hotmail.com>
2020-04-24 16:54:10 -0400

    new change

--- src/ersenOzcan/ersenOzcanClass.java
+++ src/ersenOzcan/ersenOzcanClass.java
@@ -4,6 +4,24 @@ public class ersenOzcanClass {
     public static void main(String[] args) {
         System.out.println("my name is ersen ozcan");
         System.out.println("my name is ersen ozcan");
+        /*
+
+ 1. create a project
+ 2. ceated a local(git) repository  --> git init
+ 3.write some code / created a class
+ 3. Add files to staging are --> git add .
+ 4. Commit for snapshot-> git commit -m"add filename"
+ 5. Created remote repo --> GitHub (+)
+ 6. copy paste 2 commads to Terminal
+     git remote add origin URL
+     git push -u orgin master
+ 7.Go to github refresh the page to see the project
+ 8. From github , open a class and edit it
+ 9. click commit changes
+ 10. From IntelliJ terminal --> git pull
+
+
+         */
         
     }
 
