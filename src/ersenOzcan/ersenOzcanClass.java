bbebd0f35e8a6ff8c8bb9b1aa33c12e3f3c52eb2
ersen1983 <64223825+ersen1983@users.noreply.github.com>
2020-04-24 16:49:55 -0400

    Update ersenOzcanClass.java

--- src/ersenOzcan/ersenOzcanClass.java
+++ src/ersenOzcan/ersenOzcanClass.java
@@ -3,6 +3,8 @@ package ersenOzcan;
 public class ersenOzcanClass {
     public static void main(String[] args) {
         System.out.println("my name is ersen ozcan");
+        System.out.println("my name is ersen ozcan");
+        
     }
 
 }
